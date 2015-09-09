81ef40dfd96a979cabdaee862227bdc9b574fbe7
scaumiao <scaumiao@gmail.com>
2015-09-09 17:21:46 +0800

    Initial Commit

---
+++ MusicApp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MusicApp
+//
+//  Created by 许汝邈 on 15/9/9.
+//  Copyright © 2015年 miao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
